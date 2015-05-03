be7af2eb29325853a943a39092e5a9fca54e1ef2
1ems <lems@gmx.net>
2015-05-03 09:11:47 +0200

    Run update-resolv-no-vpn

--- autostart.sh
+++ autostart.sh
@@ -1,11 +1,8 @@
 #! /bin/sh
-#	$Id: autostart.sh,v 1.8 2015/04/19 07:01:57 lems Exp $
+#	$Id: autostart.sh,v 1.9 2015/05/03 07:11:33 lems Exp $
 #
 # Copy this to /storage/.config and make it executable
 
 # Update /etc/resolv.conf with alternative DNS servers.
-# Not needed by me since I force them on my router anyway.
-#/storage/.config/vpn/update-resolv-no-vpn &
-
-# Set up VPN connection.
-/storage/.config/vpn/update-resolv &
+# (Not needed if you force them on your router anyway.)
+/storage/.config/vpn/update-resolv-no-vpn &
