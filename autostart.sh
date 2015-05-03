e21172bfb79819f82e59fb39cdf1ad1159603383
1ems <lems@gmx.net>
2015-05-03 09:20:06 +0200

    Start the VPN connection

--- autostart.sh
+++ autostart.sh
@@ -1,8 +1,11 @@
 #! /bin/sh
-#	$Id: autostart.sh,v 1.9 2015/05/03 07:11:33 lems Exp $
+#	$Id: autostart.sh,v 1.10 2015/05/03 07:19:57 lems Exp $
 #
 # Copy this to /storage/.config and make it executable
 
 # Update /etc/resolv.conf with alternative DNS servers.
 # (Not needed if you force them on your router anyway.)
 /storage/.config/vpn/update-resolv-no-vpn &
+
+# Start the VPN connection
+/storage/.config/vpn/launch-vpn.sh &
