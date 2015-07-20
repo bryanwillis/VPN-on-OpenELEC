d1226dfd4eccb38944fa58d9fca66e4348e3e700
1ems <lems@gmx.net>
2015-07-20 11:55:23 +0200

    Syntax cleanup.

--- vpn/kill-vpn.sh
+++ vpn/kill-vpn.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
-#	$Id: kill-vpn.sh,v 1.2 2015/01/12 12:35:01 lems Exp $
+#	$Id: kill-vpn.sh,v 1.3 2015/07/20 09:55:04 lems Exp $
 #
 # This is used via Advanced Launcher. It will kill the connection.
 
 VPN_PID=`pgrep openvpn`
-[ "$VPN_PID" != "" ] && kill $VPN_PID
+[ -n "$VPN_PID" ] && kill $VPN_PID
